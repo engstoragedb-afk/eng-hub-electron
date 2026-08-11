373becb03a33c833fbd3bea1582dd7e6f43091b6
mac <mac@macs-MacBook-Air-2.local>
2026-08-11 15:24:39 +0700

    chore: bump version to 1.0.2

--- renderer/common/utils/config.ts
+++ renderer/common/utils/config.ts
@@ -1,5 +1,5 @@
 export default {
   BACKEND_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL === '/api' 
-    ? 'http://127.0.0.1:3012/api' 
-    : (process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:3012/api')
+    ? 'https://d0f2-2404-c0-d001-b4b9-1b2-b826-9f6b-889f.ngrok-free.app/api' 
+    : (process.env.NEXT_PUBLIC_API_BASE_URL || 'https://d0f2-2404-c0-d001-b4b9-1b2-b826-9f6b-889f.ngrok-free.app/api')
 };
