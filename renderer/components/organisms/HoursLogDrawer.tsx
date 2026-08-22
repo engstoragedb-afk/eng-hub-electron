04e948051cfef106b892bcf15b34070738fc9d7e
mac <mac@macs-MacBook-Air-2.local>
2026-08-22 11:44:05 +0700

    update v1.3.5 ADD HISTORY UPDATE HOURS UNIT

--- renderer/components/organisms/HoursLogDrawer.tsx
+++ renderer/components/organisms/HoursLogDrawer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo, useRef } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { FaTimes, FaCalendarAlt, FaChevronLeft, FaChevronRight, FaUndo } from "react-icons/fa";
 import { ACTIONS } from "@/common/utils/action";
 import { auditLogService } from "@/services";
